866b666640430fab044485d0b17ab5abac8d4ef6
zifstark <57366738+zifstark@users.noreply.github.com>
2020-01-25 09:40:30 +0100

    Connect to Postgres

--- blog/settings.py
+++ blog/settings.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
